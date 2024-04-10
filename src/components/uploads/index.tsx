d62e716570743c00dfea9e9f381cbc714f724ec7
medsonmoombe <medsonmoombe21@gmail.com>
2024-04-10 15:32:44 -0700

    Add the upload icon

--- src/components/uploads/index.tsx
+++ src/components/uploads/index.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Grid, GridItem, Box, Image, Modal, ModalOverlay, ModalContent, ModalCloseButton, ModalBody, ModalFooter, Button, IconButton } from "@chakra-ui/react";
 import { backgrounds } from "../function";
-import { FaExpand } from "react-icons/fa";
+import { FaExpand, FaPlus } from "react-icons/fa";
 
 const images = backgrounds;
 
 const ImageGrid = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const fileInputRef = useRef(null);
 
   const handleImageClick = (index: React.SetStateAction<number>) => {
     setCurrentImageIndex(index);
@@ -26,9 +27,45 @@ const ImageGrid = () => {
     setIsOpen(false);
   };
 
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      // Process the uploaded file (e.g., upload to server)
+      console.log("Uploaded file:", file);
+    }
+  };
+
+  const handleClickPlusIcon = () => {
+    fileInputRef.current.click();
+  };
+
   return (
     <>
+
       <Grid templateColumns="repeat(2, 1fr)" gap={4} px={4} mb={'100px'}>
+      <IconButton
+            aria-label="Upload"
+            icon={<FaPlus />}
+            bg={'blue.100'}
+            width={'50px'}
+            height={'50px'}
+            borderRadius={'50%'}
+            border={'1px solid'}
+            borderColor={'gray.400'}
+            color={'blue.400'}
+            position="fixed"
+            top="75%"
+            right="15px"
+            zIndex="999"
+            onClick={handleClickPlusIcon}
+          />
+            <input
+            ref={fileInputRef}
+            type="file"
+            accept="image/png, image/jpeg"
+            style={{ display: "none" }}
+            onChange={handleImageUpload}
+          />
         {images.map((image, index) => (
           <GridItem key={index}>
             <Box
@@ -59,6 +96,7 @@ const ImageGrid = () => {
                   }}
                 />
                 </div>
+                
             </Box>
           </GridItem>
         ))}
