f331b5c018301b46b6085104d8f628e4547f0e65
medsonmoombe <medsonmoombe21@gmail.com>
2024-04-20 00:16:45 -0700

    update the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,65 @@
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+
+// // // https://vitejs.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+
+
 // import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+// import { VitePWA } from 'vite-plugin-pwa'
+// import react from '@vitejs/plugin-react-swc'
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
-//   plugins: [react()],
+//   plugins: [
+//     react(),
+//     VitePWA({
+//       registerType: 'prompt',
+//       manifest: {
+//         name: '',
+//         short_name: 'J&R Wedding App',
+//         description: 'wedding app for J&R',
+//         theme_color: '#ffffff',
+//         display: 'standalone',
+//         orientation: 'portrait',
+//         start_url: '/',
+//       },
+//     }),
+//   ],
 // })
 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'prompt',
+      registerType: 'autoUpdate', // Change to 'autoUpdate' for better user experience
       manifest: {
-        name: '',
-        short_name: 'J&R Wedding App',
-        description: 'wedding app for J&R',
+        name: 'J&R Wedding App',
+        short_name: 'J&R App',
+        description: 'Wedding app for J&R',
         theme_color: '#ffffff',
+        background_color: '#ffffff',
         display: 'standalone',
-        orientation: 'portrait',
         start_url: '/',
+        icons: [
+          {
+            src: '/J&R_icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/J&R_icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
       },
     }),
   ],
-})
+});
