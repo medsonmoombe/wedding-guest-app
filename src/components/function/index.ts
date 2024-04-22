acbb6e39ffff343b1ad40984e0a21ed0a8c2f2d8
medsonmoombe <medsonmoombe21@gmail.com>
2024-04-22 11:45:39 -0700

    Add service worker to render images offline

--- src/components/function/index.ts
+++ src/components/function/index.ts
@@ -1,4 +1,4 @@
-import img1 from '../../assets/images/img-1.jpg';
+import img1 from '../../assets/images/img-1.webp';
 import img2 from '../../assets/images/img-2.jpg';
 import img3 from '../../assets/images/img-3.jpg';
 import img4 from '../../assets/images/img-4.jpg';
