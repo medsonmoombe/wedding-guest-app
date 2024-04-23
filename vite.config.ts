38870b48e81315298b0f628efb816b18cf75195c
medsonmoombe <medsonmoombe21@gmail.com>
2024-04-23 00:10:28 -0700

    Add service worker to render images offline

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,15 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate', // Change to 'autoUpdate' for better user experience
+       // add this to cache all the imports
+       workbox: {
+        globPatterns: ["**/*"],
+    },
+    // add this to cache all the
+    // static assets in the public folder
+    includeAssets: [
+        "**/*",
+    ],
       manifest: {
         name: 'J&R Wedding App',
         short_name: 'Nosso Dia',
