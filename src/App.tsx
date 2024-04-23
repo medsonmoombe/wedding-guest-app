b950f383fe09f1ab0609957f82c492b8d535b004
medsonmoombe <medsonmoombe21@gmail.com>
2024-04-22 23:32:31 -0700

    Add service worker to render images offline

--- src/App.tsx
+++ src/App.tsx
@@ -141,17 +141,17 @@ function App() {
   }
   );
 
-  // if ('serviceWorker' in navigator) {
-  //   window.addEventListener('load', () => {
-  //     navigator.serviceWorker.register('/sw.js')
-  //       .then(registration => {
-  //         console.log('Service Worker registered with scope:', registration.scope);
-  //       })
-  //       .catch(error => {
-  //         console.error('Service Worker registration failed:', error);
-  //       });
-  //   });
-  // }
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(error => {
+          console.error('Service Worker registration failed:', error);
+        });
+    });
+  }
    
 
 
