003a0ff566be3a40b508af33c9133a3ec93f1fa1
medsonmoombe <medsonmoombe21@gmail.com>
2024-04-22 23:28:24 -0700

    Add service worker to render images offline

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,35 @@
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+
+// // // https://vitejs.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+
+
+// import { defineConfig } from 'vite'
+// import { VitePWA } from 'vite-plugin-pwa'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     VitePWA({
+//       registerType: 'prompt',
+//       manifest: {
+//         name: '',
+//         short_name: 'J&R Wedding App',
+//         description: 'wedding app for J&R',
+//         theme_color: '#ffffff',
+//         display: 'standalone',
+//         orientation: 'portrait',
+//         start_url: '/',
+//       },
+//     }),
+//   ],
+// })
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -39,4 +71,7 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    assetsInlineLimit: 200000 // or a higher value
+  }
 });
